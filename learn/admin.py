ec2fc740593948b1a2a399e20cd38451537a4f66
fanhaipeng <fanhaipeng0403@gmail.com>
2018-11-11 19:10:59 +0800

    admin的各种高级功能，权限，搜索啥的

--- learn/admin.py
+++ learn/admin.py
@@ -1,6 +1,10 @@
 # Register your models here.
 
-# 主要作用是，是将Model注册到Django自带的admin里
+
+# 这个模块的主要作用是，是将Model注册到Django自带的admin里
+# 在flask一般使用flask_admin
+# https://code.ziqiangxuetang.com/django/django-admin.html
+
 from django.contrib import admin
 
 from .models import Person
